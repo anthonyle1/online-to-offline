cf2051377c8a8e201ea5f050868eac48b2413c63
Alice Jiang <149022551+cherrypicked12@users.noreply.github.com>
2024-11-27 16:30:25 -0500

    box shadow and clickable index logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,14 @@ export default function Home() {
     // home page: 
 
     <div class="bg-white  text-center text-black h-screen justify-center">
-      <img src="./logo-subtext.png" class="h-1/6 inline-block my-9"/>
+      <a href="./">
+        <img src="./logo-subtext.png" class="h-1/6 inline-block my-9"/>
+      </a>
       <div class="align-middle">
-      <div class="mx-10 mb-6 px-56 py-28 w-auto h-auto bg-slate-200 rounded-lg border-2 border-black justify-self-center">
+      <div class="mx-10 mb-6 px-56 py-28 w-auto h-auto bg-slate-200 rounded-lg border-2 border-black justify-self-center-custom-shadow">
        really cool and fun flavor text, nothing bad to say here!
        </div>
-
+       
        <div>
         <a href="./connect">        
           <button class="xanh-mono-regular-bold title-button">
