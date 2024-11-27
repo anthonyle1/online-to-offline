3d79fd1b5765ad2239426c8cf0ed11a33f8a595a
Alice Jiang <149022551+cherrypicked12@users.noreply.github.com>
2024-11-27 16:32:57 -0500

    width issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <img src="./logo-subtext.png" class="h-1/6 inline-block my-9"/>
       </a>
       <div class="align-middle">
-      <div class="mx-10 mb-6 px-56 py-28 w-auto h-auto bg-slate-200 rounded-lg border-2 border-black justify-self-center custom-shadow">
+      <div class="mx-10 mb-6 px-56 py-28 w-auto h-auto bg-slate-200 rounded-lg border-2 border-black justify-self-center">
        really cool and fun flavor text, nothing bad to say here!
        </div>
 
