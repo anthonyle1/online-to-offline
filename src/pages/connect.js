603f555c773617e8a5f49421095e969100eb501f
anthonyle1 <leanthony2284@gmail.com>
2024-11-26 23:37:19 -0500

    added new page, don't know what to do with it just yet

---
+++ src/pages/connect.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import localFont from "next/font/local";
+import React from 'react';
+
+export default function connect() {
+  return (
+    <div class="bg-white  text-center text-black h-screen justify-center">
+      <p class="p-10 xanh-mono-regular-bold">
+        this is probably where we ask for the user to sign onto steam
+        OR import their own data (if we get to it) (not collected probably cuz privacy concerns.
+        OR WE SHOULD ASK THEM TO DO IT ON THE LANDING PAGE) </p>
+    </div>
+  );
+};
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,29 @@
 import Image from "next/image";
 import localFont from "next/font/local";
 
+
 export default function Home() {
   return (
     // home page: 
+
     <div class="bg-white  text-center text-black h-screen justify-center">
       <img src="./logo-subtext.png" class="h-1/6 inline-block my-9"/>
       <div class="align-middle">
       <div class="mx-10 mb-6 px-56 py-28 w-auto h-auto bg-slate-200 rounded-lg border-2 border-black justify-self-center">
-       getting gamers bitches in 2024
+       really cool and fun flavor text, nothing bad to say here!
        </div>
+
        <div>
-        <button class="title-button">Connect with others!</button>
-        <button class="title-button xanh-mono-regular">Join the Discord!</button>
+        <a href="./connect">        
+          <button class="xanh-mono-regular-bold title-button">
+            Connect with others!</button>
+        </a>
+        <a href="https://github.com/anthonyle1/online-to-offline">
+          <button class="xanh-mono-regular-bold title-button" >
+            View the GitHub Repo!</button>  
+        </a>
        </div>
+       
       </div>
     </div>
   );
