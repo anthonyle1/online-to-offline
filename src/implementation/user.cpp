e3bd5bbf1e5a5951ba67527a043e90ab38cb95d8
mjtady <mjtady@email.com>
2024-12-03 13:29:25 -0500

    created structs for game and user data

--- src/implementation/user.cpp
+++ src/implementation/user.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
 
 using namespace std;
 
 class UserInformation{
     private:
-    struct User{
-        
-    };
+        struct Game{
+            string gamename;
+            unsigned int steamgameid;
+            string primary_genre; // primary genre
+            string secondary_genre; //secondary genre if any
+            float timeplayed;
+        };
+        struct User{
+            string username;
+            unsigned int userid;
+            vector<Game> userlibrary; // user's game library
+            unordered_set<string> recentgenres;
+            unordered_map<string, float> timeplayedpergame; // map that tracks the time played per game
+            unordered_set<string, float> timeplayedpergenre; // map that tracks the time played per genre
+        };
+
+        unordered_map<string, vector<unsigned int>> userbygenre;
+        unordered_map<string, vector<unsigned int>> userbygame;
     public:
+
 };
